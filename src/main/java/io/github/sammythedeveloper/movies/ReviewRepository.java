a1d509428e54744e2a84f38518490dac65da993a
sammythedeveloper <justsammy484@gmail.com>
2024-11-18 21:31:37 -0500

    review setup testing

--- src/main/java/io/github/sammythedeveloper/movies/ReviewRepository.java
+++ src/main/java/io/github/sammythedeveloper/movies/ReviewRepository.java
@@ -1,4 +1,9 @@
 package io.github.sammythedeveloper.movies;
 
-public class ReviewRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
